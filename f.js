7e282d3f19a51d212a0c1459d814afe8588bb152
mad9090wsa <abdulaziz.a.a.2006@gmail.com>
2022-12-28 22:42:20 +0500

    4

--- f.js
+++ f.js
@@ -1,7 +1,7 @@
 let w1 = document.querySelector(".w1")
 let w2 = document.querySelector(".w2")
 let body = document.querySelector("body")
-let h1 = document.querySelector("h1")
+let h1 = document.querySelector("h2")
 
 w1.addEventListener("click",()=>{
     if(w1.classList == "w1"){
